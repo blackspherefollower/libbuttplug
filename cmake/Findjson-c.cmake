706302fefcdc491f4bebef7040c280c2e582c86b
blackspherefollower <darktech@black-sphere.co.uk>
2017-07-25 11:01:12 +0100

    Updating CMake libjson-c discovery

--- cmake/Findjson-c.cmake
+++ cmake/Findjson-c.cmake
@@ -5,8 +5,11 @@
 # JSONC_FOUND, If false, do not try to use libWebSockets
 #
 
-find_path(JSONC_INCLUDE_DIRS json.h)
-find_library(JSONC_LIBRARIES json-c)
+find_package(PkgConfig)
+pkg_check_modules(PC_JSON-C QUIET json-c)
+
+find_path(JSONC_INCLUDE_DIRS json.h HINTS ${PC_JSON-C_INCLUDEDIR} ${PC_JSON-C_INCLUDE_DIRS} PATH_SUFFIXES json-c json)
+find_library(JSONC_LIBRARIES NAMES json-c libjson-c HINTS ${PC_JSON-C_LIBDIR} ${PC_JSON-C_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(json-c REQUIRED_VARS JSONC_INCLUDE_DIRS JSONC_LIBRARIES)
